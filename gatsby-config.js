58703cb8ca3efd299db02328b519af33f0535241
“digital”-underground” <michael.gene.martin@gmail.com>
2024-07-16 11:43:08 -0700

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-plugin-sharp`,
   ],
 }
