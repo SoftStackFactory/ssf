cac662e5c9da8744f52a65f68daee7b2488edef0
“digital”-underground” <michael.gene.martin@gmail.com>
2024-08-28 13:27:03 -0700

    Update image paths

--- src/components/header.js
+++ src/components/header.js
@@ -82,7 +82,7 @@ function Header() {
               }}
             >
               <img
-                src='./images/logo-white.png'
+                src='../logo-white.png'
                 style={logoStyle}
                 alt="logo of softstackfactory"
               />
